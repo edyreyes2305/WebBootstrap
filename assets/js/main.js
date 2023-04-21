3ccf7b3406d0c6d349426683ec5781e838723676
Edgar Reyes <edyreyes2305@gmail.com>
2023-04-21 12:53:46 -0500

    Add javascript ejercicios

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+function changeColorToRed(){
+    const refName = document.getElementById("name");
+
+    refName.style.color = "red";
+}
+
+function changeColorTo(color){
+    const refName = document.getElementById("name");
+
+    refName.style.color = color;
+}
+
+function changeColorParagraph(color, refObj) {
+    
+    refObj.style.color = color;
+}
+
