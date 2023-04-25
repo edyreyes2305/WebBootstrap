e4975f57c1cedd3353424d8ce2cb7e5513690794
Edgar Reyes <edyreyes2305@gmail.com>
2023-04-25 18:26:50 -0500

    Impl Dime tu nombre

---
+++ assets/js/calculadora.js
@@ -0,0 +1,16 @@
+const resultado = document.getElementById("resultado");
+const boton7 = document.getElementById("boton7").value;
+const boton8 = document.getElementById("boton8").value;
+const boton9 = document.getElementById("boton9").value;
+const boton4 = document.getElementById("boton4").value;
+const boton5 = document.getElementById("boton5").value;
+const boton6 = document.getElementById("boton6").value;
+const boton1 = document.getElementById("boton1").value;
+const boton2 = document.getElementById("boton2").value;
+const boton3 = document.getElementById("boton3").value;
+
+
+
+const calculadora = (num1, num2, operacion) => {
+    let result;
+}
